42060be7b1ebf513377c05110c351e0153c662f1
Alastair Hughes <hobbitalastair@gmail.com>
2016-03-28 19:58:26 +1300

    Swapped back to using per-file makepkg calls

--- libpackage.sh
+++ libpackage.sh
@@ -4,46 +4,6 @@
 PKGEXT='.pkg.tar.xz'
 SRCEXT='.src.tar.gz'
 
-callmakepkg() {
-    # Call makepkg locally.
-    local configdir="$(realpath "$1")"
-    local prefix="$2"
-    local path="$3"
-    shift 3
-
-    pushd "${path}" > /dev/null || exit 1
-    # Generate the config file.
-    # We can't use a here-document because that gets lost somewhere when
-    # makepkg uses fakeroot.
-    local makepkgconf="$(mktemp "${TMPDIR:-/tmp}/makepkgconf.XXXXXXXX")" || \
-        error 1 "Failed to make the makepkgconf temporary file!"
-    trap "rm -f '${makepkgconf}'" EXIT
-    genmakepkgconf "${configdir}" "${prefix}" > "${makepkgconf}"
-
-    # Run makepkg.
-    makepkg --config "${makepkgconf}" "$@" || exit "$?"
-
-    # Clean up.
-    rm -f "${makepkgconf}"
-    popd > /dev/null
-}
-
-genmakepkgconf() {
-    # Print an suitable makepkg.conf.
-    # TODO: This should override some variables, sanitize others, etc...
-    #       which it clearly doesn't.
-    local configdir="$1"
-    local prefix="$2"
-    cat << EOF
-source "${configdir}/src/${prefix}/makepkg.conf"
-SRCPKGDEST="${configdir}/srctar"
-PKGDEST="${configdir}/pkgs"
-LOGDEST="${configdir}/logs"
-PKGEXT='${PKGEXT}'
-SRCEXT='${SRCEXT}'
-EOF
-}
-
 pkgdirsrctar() {
     # Print the source tarball path for the given package dir.
     local configdir="$1"
@@ -59,7 +19,7 @@ pkgdirsrctar() {
     pkgver="$(sed -n "${srcinfo}" -e '/pkgver = /p' | sed -e 's:.*= ::')"
     pkgrel="$(sed -n "${srcinfo}" -e '/pkgrel = /p' | sed -e 's:.*= ::')"
     
-    printf "${pkgbase}-${pkgver}-${pkgrel}.src.tar.gz"
+    printf "%s-%s-%s%s" "${pkgbase}" "${pkgver}" "${pkgrel}" "${SRCEXT}"
 }
 
 pkgdirpackages() {
@@ -91,5 +51,6 @@ pkgdirpackages() {
     else
         local carch="any"
     fi
-    printf "${pkgbase}-${pkgver}-${pkgrel}-${carch}${PKGEXT}"
+    printf "%s-%s-%s-%s%s" "${pkgbase}" "${pkgver}" "${pkgrel}" "${carch}" \
+        "${PKGEXT}"
 }
